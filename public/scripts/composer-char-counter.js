0988d77c8fe463e6b7ccbc3e74cca7c66a3b4573
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-27 05:21:38 +0000

    Add tweet counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,14 @@
 $(document).ready(function() {
-  $("#tweet-text").on('keypress', function() {
-    console.log(this);
+  $("#tweet-text").on('input', function() {
+    let charactersLeft = 140 - this.value.length;
+    let charCounter = $('#char-counter');
+    charCounter.text(charactersLeft);
+    if(charactersLeft < 0) {
+      charCounter.css('color', 'red');
+    } else {
+      charCounter.css('color', '#545149');
+    }
   })
 });
 
 
-// A non jquery solution:
-
-// window.addEventListener("load", (event) => {
-//   const updateCharacterCounter = (parentQuerySelector) => {
-//   document
-//     .querySelector(parentQuerySelector)
-//     .addEventListener("input", function(event) {
-//       const remainingCharacters = 140 - this.value.length;
-//       const counter = document.querySelector("counter");
-//       if (remainingCharacters >= 0) {
-//         counter.classList.remove("over-characters");
-//       } else {
-//         counter.classList.add("over-characters");
-//       }
-//       counter.value = remainingCharacters.toString();
-//     });
-//   };
-//     updateCharacterCounter(#tweet-text");
-// });
