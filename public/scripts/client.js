32cd782d6c80c5c2478e2624bec8f540731ba962
JSmith604 <jacquelinesmith190@gmail.com>
2021-01-29 00:33:37 +0000

    Fix createTweetElement function in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -36,15 +36,19 @@ const data = [
     const profilePicture = $('<img src=' + user.avatars + ' alt="' + user.name +'">');
     const profileName = $('<div class="profile">' + user.name + '</div>');
     const handle = $('<div class="user-name">' + user.handle + '</div>');
+    const tweetTop = $('<div class="tweet-top"></div');
+    const pictureName = $('<div class="picture-name"></div>');
     const content = tweetObject.content.text;
     const tweet = $('<div class="tweet">' + content + '</div>');
     const date = new Date(tweetObject.created_at);
     const footer = $('<footer></footer>');
     const dateCreated = $('<div class="date">' + date.toLocaleDateString() + '</div>');
     const icons = $('<div class="icons"><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></div>');
-    tweetDocObj.append(profilePicture);
-    tweetDocObj.append(profileName);
-    tweetDocObj.append(handle);
+    pictureName.append(profilePicture);
+    pictureName.append(profileName);
+    tweetTop.append(pictureName);
+    tweetTop.append(handle);
+    tweetDocObj.append(tweetTop);
     tweetDocObj.append(tweet);
     footer.append(dateCreated);
     footer.append(icons);
@@ -72,4 +76,29 @@ const data = [
     console.log(result);
   })
 })
+
+//Previous version
+
+// $(document).ready(function() {
+//   const createTweetElement = function(tweetObject) {
+//    const tweetDocObj = $('<article class="user-tweet"></article>');
+//    const user = tweetObject.user;
+//    const profilePicture = $('<img src=' + user.avatars + ' alt="' + user.name +'">');
+//    const profileName = $('<div class="profile">' + user.name + '</div>');
+//    const handle = $('<div class="user-name">' + user.handle + '</div>');
+//    const content = tweetObject.content.text;
+//    const tweet = $('<div class="tweet">' + content + '</div>');
+//    const date = new Date(tweetObject.created_at);
+//    const footer = $('<footer></footer>');
+//    const dateCreated = $('<div class="date">' + date.toLocaleDateString() + '</div>');
+//    const icons = $('<div class="icons"><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></div>');
+//    tweetDocObj.append(profilePicture);
+//    tweetDocObj.append(profileName);
+//    tweetDocObj.append(handle);
+//    tweetDocObj.append(tweet);
+//    footer.append(dateCreated);
+//    footer.append(icons);
+//    tweetDocObj.append(footer);
+//    return tweetDocObj;
+//   }
 
